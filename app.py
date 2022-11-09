747b1e86296fc11c274ab4f238491813a83b6242
donadivarun <donadivarun@gmail.com>
2022-11-09 10:34:38 +0100

    Adding and getting events works! TODO: Updation and deletion.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, g
 from flask_restful import Api
 from routes import Event, EventList
 from flask_cors import CORS
+import os
+from psycopg2 import pool
 
 
 app = Flask(__name__)
@@ -9,9 +11,25 @@ CORS(app)
 api = Api(app)
 BASE_URL = '/api'
 
+host = os.environ['HOST']
+database = os.environ['DATABASE']
+db_port = os.environ['DB_PORT']
+user = os.environ['USER']
+password = os.environ['PASSWORD']
+MIN = os.environ['MIN']
+MAX = os.environ['MAX']
+
+app.config['pSQL_pool'] = pool.SimpleConnectionPool(MIN, MAX, host=host, database=database, port=db_port, user=user, password=password)
 api.add_resource(EventList, f'{BASE_URL}/Events')
 api.add_resource(Event, f'{BASE_URL}/Events/<event_id>', f'{BASE_URL}/eventbytitle/<title>')
 
+
+@app.teardown_appcontext
+def close_conn(e):
+    db = g.pop('db', None)
+    if db is not None:
+        app.config['pSQL_pool'].putconn(db)
+        print('released connection back to pool')
 
 if __name__ == '__main__':
     app.run(debug=True, port=32413)
--- repository.py
+++ repository.py
@@ -1,6 +1,6 @@
 from models import EventModel, UserModel
 import psycopg2
-import os
+from flask import current_app, g
 
 # user1 = UserModel(1, 'Lalit')
 # user2 = UserModel(2, 'Max')
@@ -9,118 +9,87 @@ import os
 # event3 = EventModel(3, 'Event 3')
 # event4 = EventModel(4, 'Event 4')
 
-host = os.environ['HOST']
-database = os.environ['DATABASE']
-db_port = os.environ['DB_PORT']
-user = os.environ['USER']
-password = os.environ['PASSWORD']
+# host = os.environ['HOST']
+# database = os.environ['DATABASE']
+# db_port = os.environ['DB_PORT']
+# user = os.environ['USER']
+# password = os.environ['PASSWORD']
 
 
 class Repository:
     def get_all_events(self):
-        conn = None
         event_list = []
-        try:
-            conn = self.get_db()
-            if conn:
-                ps_cursor = conn.cursor()
-                ps_cursor.execute("Select event_id, title, image, username from events")
-                event_records = ps_cursor.fetchall()
-                for row in event_records:
-                    event_list.append(EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3]))
-                ps_cursor.close()
-            return event_list
+        conn = self.get_db()
+        if conn:
+            ps_cursor = conn.cursor()
+            ps_cursor.execute("Select event_id, title, image, username from events")
+            event_records = ps_cursor.fetchall()
+            for row in event_records:
+                event_list.append(EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3]))
+            ps_cursor.close()
+        return event_list
 
-        except Exception as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
 
     def get_event_by_id(self, event_id):
-        conn = None
-        try:
-            conn = self.get_db()
-            event = None
-            if conn:
-                ps_cursor = conn.cursor()
-                ps_cursor.execute(f"Select event_id, title, image, username from events where event_id = %d;", (event_id))
-                event_records = ps_cursor.fetchall()
-                if len(event_records) < 1:
-                    print("Event not found, check your id.")
-                    ps_cursor.close()
-                    return None
-                for row in event_records:
-                    event = EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3])
+        conn = self.get_db()
+        event = None
+        if conn:
+            ps_cursor = conn.cursor()
+            ps_cursor.execute(f"Select event_id, title, image, username from events where event_id = %d;", (event_id))
+            event_records = ps_cursor.fetchall()
+            if len(event_records) < 1:
+                print("Event not found, check your id.")
                 ps_cursor.close()
-            return event
-        except Exception as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
+                return None
+            for row in event_records:
+                event = EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3])
+            ps_cursor.close()
+        return event
 
     def get_event_by_title(self, title):
-        conn = None
-        try:
-            conn = self.get_db()
-            event = None
-            if conn:
-                ps_cursor = conn.cursor()
-                title += '%'
-                ps_cursor.execute(f"Select event_id, title, image, username from events where title similar to %s", (title))
-                event_records = ps_cursor.fetchall()
-                if len(event_records) < 1:
-                    print("Event not found, check your title.")
-                    ps_cursor.close()
-                    return None
-                for row in event_records:
-                    event = EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3])
+        conn = self.get_db()
+        event = None
+        if conn:
+            ps_cursor = conn.cursor()
+            title += '%'
+            ps_cursor.execute(f"Select event_id, title, image, username from events where title similar to %s", (title))
+            event_records = ps_cursor.fetchall()
+            if len(event_records) < 1:
+                print("Event not found, check your title.")
                 ps_cursor.close()
-            return event
-        except Exception as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
+                return None
+            for row in event_records:
+                event = EventModel(id=row[0], title=row[1], likes=0, image=row[2], user_id=row[3])
+            ps_cursor.close()
+        return event
+
 
     def get_db(self):
-        return psycopg2.connect(
-            host=host,
-            database=database,
-            port=db_port,
-            user=user,
-            password=password)
+        if 'db' not in g:
+            g.db = current_app.config['pSQL_pool'].getconn()
+        return g.db
 
     def add_event(self, data):
-        conn = None
-        try:
-            conn = self.get_db()
-            event = None
-            if conn:
-                ps_cursor = conn.cursor()
-                if 'username' not in data: # remove this!
-                    data['username'] ='Admin'
-                if 'image' not in data:
-                    data['image'] = ''
-                # print(data)
-                ps_cursor.execute(f"INSERT INTO events (title, username, image) VALUES (%s, %s, %s) RETURNING event_id", (data['title'], data['username'], data['image']))
-                conn.commit()
-                event_id = ps_cursor.fetchone()[0]
-                if event_id is None:
-                    ps_cursor.close()
-                    print(event_id)
-                    print("insertion unsuccessful")
-                    return None
+        conn = self.get_db()
+        event = None
+        if conn:
+            ps_cursor = conn.cursor()
+            if 'username' not in data: # remove this!
+                data['username'] ='Admin'
+            if 'image' not in data:
+                data['image'] = ''
+            # print(data)
+            ps_cursor.execute(f"INSERT INTO events (title, username, image) VALUES (%s, %s, %s) RETURNING event_id", (data['title'], data['username'], data['image']))
+            conn.commit()
+            event_id = ps_cursor.fetchone()[0]
+            if event_id is None:
                 ps_cursor.close()
-                event = EventModel(id=event_id, title=data['title'], likes=0, image=data['image'], user_id=data['username'])
-            return event
+                print(event_id)
+                print("insertion unsuccessful")
+                return None
+            ps_cursor.close()
+            event = EventModel(id=event_id, title=data['title'], likes=0, image=data['image'], user_id=data['username'])
+        return event
 
-        except Exception as error:
-            print(error)
-
-        finally:
-            if conn is not None:
-                conn.close()
 
 # if __name__ == '__main__':
